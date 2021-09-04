7c6754acdd0bb25156dac545d0fc3e6a33fe8a48
Johannes Ebner <Structed@users.noreply.github.com>
2021-09-04 02:26:23 +0200

    Added handling for clicking left mouse, subtract shells

--- Main.gd
+++ Main.gd
@@ -4,6 +4,8 @@ export (PackedScene) var Mob
 var score
 var rng = RandomNumberGenerator.new()
 
+const INITIAL_SHELLS = 5
+var current_shells = INITIAL_SHELLS
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,3 +25,13 @@ func game_over():
 func _on_MobTimer_timeout():
 	var mob = Mob.instance()
 	add_child(mob)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				current_shells -= 1
+				print("Left button was clicked at ", event.position)
+				print("Shells left: ", current_shells)
+			else:
+				print("Left button was released")
